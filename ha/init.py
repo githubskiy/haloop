c65726c5e02ca0c56440bf206f9e88320bcb6aa1
githubskiy <qwert66y123@gmail.com>
2024-05-12 12:10:22 +0300

    test1

--- ha/init.py
+++ ha/init.py
@@ -267,7 +267,8 @@ class Initializer:
 
         if args.init:
             checkpoint = torch.load(args.init[0], map_location=args.device)
-            module.load_state_dict(checkpoint['model'])
+            print(checkpoint.keys())
+            module.load_state_dict(checkpoint)
             if len(args.init) > 1:
                 log('averaging models')
                 avg_model = torch.optim.swa_utils.AveragedModel(module)
